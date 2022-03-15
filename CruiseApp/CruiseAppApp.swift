b499e6228bb4f4675cb2bd0e5e04dff4847211cc
tanavsharma <sharma.tanav95@gmail.com>
2022-03-15 17:27:44 -0400

    Initial Commit

---
+++ CruiseApp/CruiseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CruiseAppApp.swift
+//  CruiseApp
+//
+//  Created by Tanav Sharma on 2022-03-15.
+//
+
+import SwiftUI
+
+@main
+struct CruiseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
